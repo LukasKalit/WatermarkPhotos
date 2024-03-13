2b595167f8b89d31c55704cbb8113d23d5f9ba25
LukasKalit <lukaskalit@gmail.com>
2024-03-13 19:14:13 +0100

    Cleaning up. README file add.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple program in tkinter to automaticly add watermark to photos
+
+## Instruction:
+1. Add in the same file where is your program a watermark as "watermark.png" 
+2. Lunch a program and choose where the watermark should be
+3. Load a file/files
+4. Click "Execute" and the file "converted_photos" will be created if no exist in the place where program is and paste there watermarked photos.
--- main.py
+++ main.py
@@ -7,10 +7,8 @@ from PIL import Image
 import os
 
 
-import random
-
 WATERMARK_NAME = "watermark.png"
-
+size_scalar = float(0.2)
 def load_image(file_name):
 
     print(file_name)
@@ -24,13 +22,11 @@ def load_image(file_name):
             list_of_im.append(im)
         return list_of_im
     
-    print(im.format, im.size, im.mode)
 
 def load_watermark(watermark_name):
 
     im = Image.open(watermark_name)
     return im
-    print(im.format, im.size, im.mode)
 
 def add_watermark(list_of_paths, watermark, position_text):
 
@@ -54,48 +50,68 @@ def add_watermark(list_of_paths, watermark, position_text):
 
     for base_image in list_of_im:
 
-        # Extract the original filename from the full path
-        original_filename = os.path.splitext(os.path.basename(base_image.filename))[0]
-
-        print(base_image.filename)
-        # Construct the new filename with the "watermarked" suffix
-        output_image_path = f"converted_photos/{original_filename}_watermarked.jpg"
 
         # Resize watermark to fit on the base image
         width, height = base_image.size
-        new_width = int(width * 0.25)
-        new_height = int(new_width / aspect_ratio)
-        watermark_resized = watermark.resize((new_width, new_height))
+        if width > height:
+            new_width = int(width * size_scalar)
+            new_height = int(new_width / aspect_ratio)
+            watermark_resized = watermark.resize((new_width, new_height))
+        else:
+            new_width = int(width * size_scalar * (1.8))
+            new_height = int(new_width / aspect_ratio)
+            watermark_resized = watermark.resize((new_width, new_height))
+
 
         # Paste the watermark onto the base image
         if position_text == "center":
             label3.config(text="")
             print("position_center")
             position = ((width - new_width) // 2, (height - new_height) // 2)
+
         elif position_text == "bottomright":
             label3.config(text="")
             print("position_bottomright")
             position = (width - new_width, height - new_height)
+
+        elif position_text == "bottomcenter":
+            label3.config(text="")
+            print("position_bottomcenter")
+            position = ((width - new_width) // 2, height - new_height)
+
+        elif position_text == "bottomleft":
+            label3.config(text="")
+            print("position_bottomleft")
+            position = (0, height - new_height)
+
         else:
             label3.config(text="Invalid position")
             raise ValueError("Invalid position")
 
-        print(base_image, "1")
-        print(output_image_path, "2")
-        print( watermark, "3")
-        print(position, "4")
         base_image.paste(watermark_resized, position, watermark_resized)
 
+        # Extract the original filename from the full path
+        original_filename = os.path.splitext(os.path.basename(base_image.filename))[0]
+
+        print(base_image.filename)
+        # Construct the new filename with the "watermarked" suffix
+        output_image_path = os.path.join("converted_photos", f"{original_filename}_watermarked.jpg")
+
+
+
         # Save the final image
-        print(f"wykonuje się nie wiem po co   {output_image_path}")
+        output_directory = os.path.dirname(output_image_path)
+        os.makedirs(output_directory, exist_ok=True)
+        current_directory = os.path.dirname(__file__)
+        current_directory = os.path.join(current_directory,"..", output_image_path)
         try:
-            base_image.save(output_image_path)
+            base_image.save(current_directory)
         except FileNotFoundError:
             current_directory = os.path.dirname(__file__)
-            folder_name = 'converted_photos'
-            folder_path = os.path.join(current_directory, folder_name)
-            os.makedirs(folder_path)
-            base_image.save(output_image_path)
+            current_directory = os.path.join(current_directory, output_image_path)
+            base_image.save(current_directory)
+
+
 
 def open_filedialog(label2):
 
@@ -117,6 +133,11 @@ def open_filedialog(label2):
 def print_filename(filename):
     print(filename)
 
+def on_slider_change(value):
+    global size_scalar
+    label4.config(text=f"Size of watermark: {int(float(value))}%")
+    size_scalar = int(float(value))/100
+
 
 # Initialize Tkinter
 root = Tk()
@@ -129,14 +150,22 @@ location = StringVar()
 label1 = ttk.Label(frm, text='Where insert watermark?').grid(column=0, row=0)
 
 center = ttk.Radiobutton(frm, text='Middle', variable=location, value='center').grid(column=0, row=1)
-bottomright = ttk.Radiobutton(frm, text='Right-Bottom', variable=location, value='bottomright')
-bottomright.grid(column=0, row=2)
+bottomright = ttk.Radiobutton(frm, text='Bottom-Right', variable=location, value='bottomright').grid(column=0, row=2)
+bottomcenter = ttk.Radiobutton(frm, text='Bottom-Center', variable=location, value='bottomcenter').grid(column=1, row=1)
+bottomleft = ttk.Radiobutton(frm, text='Bottom-Left', variable=location, value='bottomleft').grid(column=1, row=2)
+
 
 label2 = ttk.Label(frm, text="no file selected")
 label2.grid(column=0, row=3)
 
 label3 = ttk.Label(frm, text="")
 label3.grid(column=0, row=4)
+
+label4 = ttk.Label(frm, text=f"Size of watermark: 20%")
+label4.grid(column=1, row=3)
+
+slider = ttk.Scale(frm, from_=1, to=50, orient=tkinter.HORIZONTAL, command=on_slider_change, value=20)
+slider.grid(row=4, column=1, padx=10)
 
 button2 = ttk.Button(frm, text="Load file", command=lambda: open_filedialog(label2))
 button2.grid(column=0, row=5)
